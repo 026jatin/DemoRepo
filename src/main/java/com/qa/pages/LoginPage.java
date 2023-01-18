1197fb8fbaf07873e7208595370b74a8a3e71549
Jatin Menaria -X (jmenaria - WIPRO LIMITED at Cisco) <jmenaria@cisco.com>
2023-01-18 15:07:18 +0530

    Modified Login page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,11 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	
+	public void loginpage() {
+		System.out.println("login page");
+	}
+	public void img() {
+		System.out.println("Image visible");
+	}
 }
